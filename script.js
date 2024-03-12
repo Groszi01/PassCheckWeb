5b3fd0dae014d22797fecdf22c242cef50c6c9a3
Groszi01 <oruak@protonmail.com>
2024-03-12 09:56:28 +0100

    JS

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 function checkLenght() {
     const cardInput = document.getElementById('card');
     const cardInputValueLength = cardInput.value.length;
-    console.log(`The length of the value in the input with id 'card' is: ${cardInputValueLength}`);
+    console.log(`Lenght: ${cardInputValueLength}`);
+
 }
 
 
